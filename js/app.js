f45b54e7be6e9f932e7127e2d653ca4b75da6f81
Anastasia Shpit <imshpit@gmail.com>
2020-12-15 23:49:38 +0200

    Menu animation finished

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-const spans = document.querySelectorAll('.menu__btn span')
 const toggleBtn = document.querySelector('.menu__btn')
+const spans = document.querySelectorAll('.menu__btn span')
+const menuList = document.querySelector('.menu__list')
 
 toggleBtn.addEventListener('click', () => {
     for (let i = 0; i < spans.length; i ++) {
         spans[i].classList.toggle('active')
     }
+
+    menuList.classList.toggle('active');
 })
