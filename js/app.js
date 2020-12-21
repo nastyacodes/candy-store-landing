a9b85575c5a51813777eb9ed5b044958a24eb086
Anastasia Shpit <imshpit@gmail.com>
2020-12-21 13:51:20 +0200

    first varsion of footer and small bugs fixed

--- js/app.js
+++ js/app.js
@@ -9,3 +9,38 @@ toggleBtn.addEventListener('click', () => {
 
     menuList.classList.toggle('active');
 })
+
+const icons = document.querySelectorAll('.footer__socials-icon')
+
+for(let i = 0; i < icons.length; i++) {
+    icons[i].addEventListener('mouseover',makeIconActive);
+    icons[i].addEventListener('click', () => {
+        makeIconActive();
+        setTimeout(makeIconInActive(), 1000);
+    });
+    icons[i].addEventListener('mouseout', makeIconInActive);
+}
+
+function makeIconActive(e) {
+    e.target.classList.add('active');
+    const img = e.target.querySelector('.footer__socials-img');
+
+    if (img) {
+        img.classList.add('active');
+    } else {
+        const div = e.target.closest('.footer__socials-icon');
+        div.classList.add('active');
+    }
+}
+
+function makeIconInActive(e) {
+    e.target.classList.remove('active');
+    const img = e.target.querySelector('.footer__socials-img');
+
+    if (img) {
+       img.classList.remove('active');
+    } else {
+        const div = e.target.closest('.footer__socials-icon');
+        div.classList.remove('active');
+    }
+}
