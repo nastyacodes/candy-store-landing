62e2333d8f11cd5ef777c3fc56c634bff333dbe6
Anastasia Shpit <imshpit@gmail.com>
2020-12-21 20:47:56 +0200

    Adding meaningful comments to .js ans .css files

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Open menu on click toggle button
 const toggleBtn = document.querySelector('.menu__btn')
 const spans = document.querySelectorAll('.menu__btn span')
 const menuList = document.querySelector('.menu__list')
@@ -10,37 +11,37 @@ toggleBtn.addEventListener('click', () => {
     menuList.classList.toggle('active');
 })
 
+//Activate social media icons on hover and click
 const icons = document.querySelectorAll('.footer__socials-icon')
 
 for(let i = 0; i < icons.length; i++) {
-    icons[i].addEventListener('mouseover',makeIconActive);
-    icons[i].addEventListener('click', () => {
-        makeIconActive();
-        setTimeout(makeIconInActive(), 1000);
-    });
-    icons[i].addEventListener('mouseout', makeIconInActive);
+    icons[i].addEventListener('mouseover',makeIconActive)
+    icons[i].addEventListener('click', makeIconActive)
+    icons[i].addEventListener('mouseout', makeIconInActive)
 }
 
+//Adding class .active to a and img tags
 function makeIconActive(e) {
     e.target.classList.add('active');
-    const img = e.target.querySelector('.footer__socials-img');
+    const img = e.target.querySelector('.footer__socials-img')
 
     if (img) {
-        img.classList.add('active');
+        img.classList.add('active')
     } else {
-        const div = e.target.closest('.footer__socials-icon');
-        div.classList.add('active');
+        const div = e.target.closest('.footer__socials-icon')
+        div.classList.add('active')
     }
 }
 
+//Removing class .active from a and img tags
 function makeIconInActive(e) {
-    e.target.classList.remove('active');
-    const img = e.target.querySelector('.footer__socials-img');
+    e.target.classList.remove('active')
+    const img = e.target.querySelector('.footer__socials-img')
 
     if (img) {
-       img.classList.remove('active');
+       img.classList.remove('active')
     } else {
-        const div = e.target.closest('.footer__socials-icon');
-        div.classList.remove('active');
+        const div = e.target.closest('.footer__socials-icon')
+        div.classList.remove('active')
     }
 }
